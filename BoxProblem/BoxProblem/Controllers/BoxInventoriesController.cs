36976a28494ccf6d9728471dbba664101ac7967b
Gene Lee <leexgenex@gmail.com>
2018-06-27 10:03:46 -0500

    create, edit in controller

---
+++ BoxProblem/BoxProblem/Controllers/BoxInventoriesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BoxProblem.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoxProblem.Controllers
+{
+    public class BoxInventoriesController : Controller
+    {
+        private BoxInventoryService service;
+
+        public BoxInventoriesController(ApplicationDbContext context)
+        {
+            service = new BoxInventoryService(context);
+        }
+
+        public IActionResult Index()
+        {
+            return View(service.GetAllBoxes());
+        }
+
+        public ActionResult Detail(int id)
+        {
+            BoxInventory box = service.GetBoxById(id);
+            return View(box);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BoxInventory box)
+        {
+            if (ModelState.IsValid)
+            {
+                service.AddBoxInventory(box);
+                return RedirectToAction("Index");
+            }
+
+            return View(box);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            BoxInventory box = service.GetBoxById(id);
+            return View(box);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BoxInventory box)
+        {
+            if (ModelState.IsValid)
+            {
+                service.SaveEdits(box);
+                return RedirectToAction("Detail");
+            }
+
+            return View(box);
+        }
+    }
+}
