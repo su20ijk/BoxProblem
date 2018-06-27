2a14d26d00dd7dd014f9d5f134b50a8eda954818
su20 <19liuq@elmbrookstudents.org>
2018-06-27 10:08:25 -0500

    backend done

---
+++ BoxProblem/BoxProblem/Repository/BoxInventoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using BoxProblem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoxProblem.Repository
+{
+    public class BoxInventoryRepository
+    {
+        private ApplicationDbContext context;
+        public BoxInventoryRepository(ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        public List<BoxInventory> GetAllBoxes()
+        {
+            return context.Boxes.ToList();
+        }
+        public BoxInventory GetBoxById(int id)
+        {
+            return context.Boxes.Find(id);
+        }
+        public void AddBox(BoxInventory toAdd)
+        {
+            context.Boxes.Add(toAdd);
+            context.SaveChanges();
+        }
+        public void EditBox(BoxInventory toEdit)
+        {
+            context.Entry(toEdit).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+        }
+        public void DeleteBox(BoxInventory toDelete)
+        {
+            context.Boxes.Remove(toDelete);
+            context.SaveChanges();
+        }
+    }
+}
